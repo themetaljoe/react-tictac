e1dbe025aa7a129a4e65adc63390802fc18b32af
Marcus Chevalier <CueBall@Marcuss-MacBook-Pro.local>
2017-06-07 18:45:46 -0500

    recommit

--- client/main.jsx
+++ client/main.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+export default class Game extends React.Component{
+    constructor(props)
+    super(props)
+    this.state = {};
+
+    render{
+        return(
+            
+        );
+    }
+}
